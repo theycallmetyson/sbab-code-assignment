0bedee5fb47df4009f5c25fb5f3f5034c4e21ff5
Tyson La <tyson.la@tacton.com>
2023-04-04 03:28:44 +0200

    added json annotations

--- src/main/java/se/tcmt/sbab/busline/models/Line.java
+++ src/main/java/se/tcmt/sbab/busline/models/Line.java
@@ -1,13 +1,26 @@
 package se.tcmt.sbab.busline.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity
+@JsonPropertyOrder({"LineNumber", "LineDesignation", "DefaultTransportMode", "DefaultTransportModeCode", "LastModifiedUtcDateTime", "ExistsFromDate"})
 public class Line {
+    @Id
+    @JsonProperty("LineNumber")
     private int lineNumber;
+    @JsonProperty("LineDesignation")
     private String lineDesignation;
+    @JsonProperty("DefaultTransportMode")
     private String defaultTransportMode;
+    @JsonProperty("DefaultTransportModeCode")
     private String defaultTransportModeCode;
+    @JsonProperty("LastModifiedUtcDateTime")
     private String lastModifiedUtcDateTime;
+    @JsonProperty("ExistsFromDate")
     private String existsFromDate;
 }
