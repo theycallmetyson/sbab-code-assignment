7db5b0d185d4da760892e5189120366a946c09ef
Tyson La <79940243+theycallmetyson@users.noreply.github.com>
2023-04-06 23:46:41 +0200

    Fetch top bus lines (#13)
    * add fetching bus lines by rank number
    
    * add fetching bus lines by top rank number and show the stop point info
    
    * refactored

--- src/main/java/se/tcmt/sbab/busline/controllers/LineController.java
+++ src/main/java/se/tcmt/sbab/busline/controllers/LineController.java
@@ -3,14 +3,14 @@ package se.tcmt.sbab.busline.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import se.tcmt.sbab.busline.models.JourneyPatternPointOnLine;
-import se.tcmt.sbab.busline.models.Line;
-import se.tcmt.sbab.busline.models.StopPoint;
 import se.tcmt.sbab.busline.services.LineService;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api/v1", produces = "application/json")
@@ -20,17 +20,7 @@ public class LineController {
     private LineService lineService;
 
     @GetMapping("/lines")
-    public Collection<Line> getAllLines() throws IOException {
-        return lineService.getAllBusLines();
-    }
-
-    @GetMapping("/stops")
-    public Collection<StopPoint> getAllStops() throws IOException {
-        return lineService.getAllBusStops();
-    }
-
-    @GetMapping("/journeypatterns")
-    public Collection<JourneyPatternPointOnLine> getAllJourneyPatterns() throws IOException {
-        return lineService.getAllBusJourneyPatterns();
+    public Map<String, Collection<?>> getAllLines(@RequestParam(name = "top") Optional<Integer> topRanks) throws IOException {
+        return lineService.getAllBusLines(topRanks);
     }
 }
--- src/main/java/se/tcmt/sbab/busline/services/LineService.java
+++ src/main/java/se/tcmt/sbab/busline/services/LineService.java
@@ -10,11 +10,11 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import se.tcmt.sbab.busline.models.BaseModel;
 import se.tcmt.sbab.busline.models.JourneyPatternPointOnLine;
-import se.tcmt.sbab.busline.models.Line;
 import se.tcmt.sbab.busline.models.StopPoint;
 
 import java.io.IOException;
-import java.util.Collection;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class LineService {
@@ -30,23 +30,58 @@ public class LineService {
     private final OkHttpClient client = new OkHttpClient();
 
     @Cacheable("lines")
-    public Collection<Line> getAllBusLines() throws IOException {
-        return fetchData(lineUrl, new TypeReference<>() {
-        });
+    public Map<String, Collection<?>> getAllBusLines(Optional<Integer> topRanks) throws IOException {
+        if (topRanks.isPresent()) {
+            return getAllStopPointsGroupedByLineNumber(topRanks.get());
+        } else {
+            return Collections.singletonMap("Lines", fetchData(lineUrl, new TypeReference<>() {
+            }));
+        }
     }
 
-    @Cacheable("stops")
-    public Collection<StopPoint> getAllBusStops() throws IOException {
+    private Collection<StopPoint> getAllBusStops() throws IOException {
         Collection<StopPoint> stopPoints = fetchData(stopUrl, new TypeReference<>() {
         });
         stopPoints.removeIf(stopPoint -> !stopPoint.getStopAreaTypeCode().equals("BUSTERM"));
         return stopPoints;
     }
 
-    @Cacheable("journeypatterns")
-    public Collection<JourneyPatternPointOnLine> getAllBusJourneyPatterns() throws IOException {
+    private Collection<JourneyPatternPointOnLine> getAllBusJourneyPatterns() throws IOException {
         return fetchData(journeyUrl, new TypeReference<>() {
         });
+    }
+
+    private Map<String, Collection<JourneyPatternPointOnLine>> getBusJourneyPatternsByTopRank(int topRanks) throws IOException {
+        Collection<JourneyPatternPointOnLine> journeyPatterns = getAllBusJourneyPatterns();
+
+        return journeyPatterns.stream()
+                .collect(Collectors.groupingBy(JourneyPatternPointOnLine::getLineNumber))
+                .entrySet().parallelStream()
+                .sorted(Comparator.comparingInt(entry -> -entry.getValue().size()))
+                .limit(topRanks)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (v1, v2) -> v1, LinkedHashMap::new));
+    }
+
+    private Map<String, Collection<?>> getAllStopPointsGroupedByLineNumber(Integer topRanks) throws IOException {
+        Collection<StopPoint> allStopPoints = getAllBusStops();
+        Map<String, Collection<JourneyPatternPointOnLine>> topJourneyPatterns = getBusJourneyPatternsByTopRank(topRanks);
+        Map<String, Collection<?>> stopPointsGroupedByTopLineNumbers = new HashMap<>();
+
+        for (Map.Entry<String, Collection<JourneyPatternPointOnLine>> entry : topJourneyPatterns.entrySet()) {
+            Collection<StopPoint> stopPoints = new ArrayList<>();
+            Collection<JourneyPatternPointOnLine> journeyPatterns = entry.getValue();
+
+            for (JourneyPatternPointOnLine journeyPattern : journeyPatterns) {
+                List<String> stopPointNumbers = Arrays.asList(journeyPattern.getJourneyPatternPointNumber().split(","));
+                for (StopPoint stopPoint : allStopPoints) {
+                    if (stopPointNumbers.contains(String.valueOf(stopPoint.getStopPointNumber()))) {
+                        stopPoints.add(stopPoint);
+                    }
+                }
+            }
+            stopPointsGroupedByTopLineNumbers.put(entry.getKey(), stopPoints);
+        }
+        return stopPointsGroupedByTopLineNumbers;
     }
 
     private <T> Collection<T> fetchData(String url, TypeReference<BaseModel<T>> typeReference) throws IOException {
