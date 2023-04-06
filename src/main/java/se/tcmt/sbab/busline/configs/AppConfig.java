c8dd4cdc3b777e42986a8e9d1ea8e1c401b2a805
Tyson La <79940243+theycallmetyson@users.noreply.github.com>
2023-04-06 15:30:51 +0200

    implemented caching for fetch calls (#10)

--- src/main/java/se/tcmt/sbab/busline/configs/AppConfig.java
+++ src/main/java/se/tcmt/sbab/busline/configs/AppConfig.java
@@ -1,7 +1,9 @@
 package se.tcmt.sbab.busline.configs;
 
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableCaching
 public class AppConfig {
 }
